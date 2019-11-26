786ef2ec8cc949cc4da75456f8130dee8da37574
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2019-11-26 09:00:53 -0200

    * Criação de funções para ativação de link. * Ajustes de layout.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,25 @@
-console.log('aplicação iniciada');
+function activate(page) {
+  if (document.querySelector(`a[href="${page}"]`)) {
+    document.querySelector(`a[href="${page}"]`).classList.add('active');
+    return;
+  }
+
+  document.querySelector(`a[href="/"]`).classList.add('active');
+}
+
+function getCurrentPage() {
+  const url = window.location.href;
+  let page = url.substr(0, url.length - 1);
+  page = page.substr(page.lastIndexOf('/'), page.length);
+}
+
+function setActiveLink () {
+  const page = getCurrentPage();
+  const activeLink = document.querySelector('.header nav.main-menu > ul li > a.active');
+
+  if (activeLink) {
+    activeLink.classList.remove('active');
+  }
+
+  activate(page);
+}
